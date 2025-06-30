7fcc3a2ffaba098f2a638b8f42b09445be545074
gerocarullo <gcarullo@fi.uba.ar>
2025-06-30 14:57:05 -0300

    Solucionado el error de los puertos + error con createDegree

--- api/handlers/degrees_routes.js
+++ api/handlers/degrees_routes.js
@@ -8,17 +8,18 @@ router.post('/', validateCreateDegree, async (req, res) => {
     const result = await createDegree(req.body)
 
     if (!result.status) {
-        res.status(200).json(res)
+        res.status(200).json(result)
     } else {
-        res.status(400).json(res.error)
+        res.status(400).json(result.error)
     }
 })
 
 router.get('/', async (req,res) => {
+    console.log(req)
 
     const result = await getAllDegrees();
     if (!result.length) {
-        res.status(404).json({"error":"no se encontro el/los usuario/s buscado/s"})
+        res.status(404).json({"error":"no se encontro la/s carrera/s buscada/s"})
     } else {
         res.status(200).json(result)
     }
