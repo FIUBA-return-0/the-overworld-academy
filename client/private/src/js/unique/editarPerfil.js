6eb30e4be46491e205dab9cb47e329b5b8606556
BernardoZmijak <bzmijak@fi.uba.ar>
2025-07-03 02:07:58 -0300

    feature: Agrego funcionalidad para permitir modificar los datos personales del perfil.

---
+++ client/private/src/js/unique/editarPerfil.js
@@ -0,0 +1,33 @@
+// inicia siempre en modo editar
+let modo = "editar";
+
+function habilitarEditarCampos(){
+    const boton = document.getElementById("editar-perfil");
+    if (modo === "editar"){
+    document.getElementById("apellido").disabled = false;
+    document.getElementById("nombre").disabled = false;
+    document.getElementById("carrera").disabled = false;
+    document.getElementById("biografia").disabled = false;
+    
+    boton.textContent = "Guardar cambios";
+    modo = "guardar";
+    }
+
+    else{
+    document.getElementById("apellido").disabled = true;
+    document.getElementById("nombre").disabled = true;
+    document.getElementById("carrera").disabled = true;
+    document.getElementById("biografia").disabled = true;
+    
+    boton.textContent = "Editar perfil";
+    modo = "editar";
+    }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    const botonEditar = document.getElementById("editar-perfil");
+    if (botonEditar) {
+        botonEditar.addEventListener("click", habilitarEditarCampos);
+    }
+});
+
