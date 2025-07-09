ff3549e498a37bdca0d29b4bf1a3929f1c8c3d19
Facundovila03 <facundovila03@gmail.com>
2025-07-09 12:07:06 -0300

    fix: cambie perfil por foto

--- api/db.sql
+++ api/db.sql
@@ -25,7 +25,7 @@ CREATE TABLE usuario (
   username VARCHAR(30) NOT NULL UNIQUE,
   carrera INT REFERENCES carreras(id),
   password TEXT NOT NULL,
-  perfil text,
+  foto text,
   bio text
 );
 ALTER TABLE carreras
