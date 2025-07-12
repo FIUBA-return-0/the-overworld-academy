024bdce2dcae6866b065f289bfb60470467e3a57
gerocarullo <gcarullo@fi.uba.ar>
2025-07-12 11:06:55 -0300

    Se arreglaron errores en los handlers

--- api/controllers/Grades/updateGrade.js
+++ api/controllers/Grades/updateGrade.js
@@ -5,13 +5,10 @@ const updateGrade = async ({ alumno, materia, description, nota }) => {
     update notas n
     set nota = $1
     where n.alumno = $2 and n.materia = $3 and n.description = $4
+    returning id
   `;
-  try {
-    const result = await db.query(query, [nota, alumno, materia, description]);
-    return { status: 0, content: result.rows[0] };
-  } catch (error) {
-    return { status: 1, content: error.detail };
-  }
+  const result = await db.query(query, [nota, alumno, materia, description]);
+  return result.rows;
 };
 
 module.exports = updateGrade;
