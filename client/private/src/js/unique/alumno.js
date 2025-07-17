e4873d4496e94ef0e8bb6d3af73815b555027f4f
Facundo I. Lattandi <facundolattandi@gmail.com>
2025-07-16 21:44:05 -0300

    fix: arreglo el link del director, todo funciona ahora y tiene handlers para todos los errores posibles en todos las peticiones. feat: crea las materias cards y las linkea con editar-materia; linkeo botón de crear materia.

--- client/private/src/js/unique/alumno.js
+++ client/private/src/js/unique/alumno.js
@@ -1,51 +1,3 @@
-function createMateriaCard(id, foto, nombre, profesor, descripcion) {
-  // materia-card
-  // materia-card-img-wrapper
-  //      materia-card-img
-  // materia-card-text-wrapper
-  //      materia-card-title
-  //      materia-card-profesor
-  //      materia-card-description
-
-  let link = document.createElement("a");
-  link.href = `/ver-materia.html?id=${id}`;
-  link.classList.add("materia-card-link");
-
-  let materiaCard = document.createElement("div");
-  materiaCard.classList.add("materia-card");
-  link.appendChild(materiaCard);
-
-  let materiaCardImgWrapper = document.createElement("div");
-  materiaCardImgWrapper.classList.add("materia-card-img-wrapper");
-  materiaCard.appendChild(materiaCardImgWrapper);
-
-  let materiaCardImg = document.createElement("img");
-  materiaCardImg.classList.add("materia-card-img");
-  materiaCardImg.src = foto;
-  materiaCardImgWrapper.appendChild(materiaCardImg);
-
-  let materiaCardTextWrapper = document.createElement("div");
-  materiaCardTextWrapper.classList.add("materia-card-text-wrapper");
-  materiaCard.appendChild(materiaCardTextWrapper);
-
-  let materiaCardTitle = document.createElement("h2");
-  materiaCardTitle.classList.add("materia-card-title");
-  materiaCardTitle.innerText = nombre;
-  materiaCardTextWrapper.appendChild(materiaCardTitle);
-
-  let materiaCardProfesor = document.createElement("h2");
-  materiaCardProfesor.classList.add("materia-card-profesor");
-  materiaCardProfesor.innerText = `Catedra: ${profesor}`;
-  materiaCardTextWrapper.appendChild(materiaCardProfesor);
-
-  let materiaCardDescription = document.createElement("p");
-  materiaCardDescription.classList.add("materia-card-description");
-  materiaCardDescription.innerText = descripcion;
-  materiaCardTextWrapper.appendChild(materiaCardDescription);
-
-  document.getElementById("materias-wrapper").appendChild(link);
-}
-
 async function fetchfillMateriasAlumno() {
   try {
     const idCarrera = localStorage.getItem("carreraID");
@@ -75,7 +27,9 @@ async function fetchfillMateriasAlumno() {
             materia.foto,
             materia.materia,
             materia.apeprofesor,
-            materia.descripcion
+            materia.descripcion,
+            "materias-wrapper",
+            "/ver-materia.html?id="
           );
         }
 
@@ -85,8 +39,9 @@ async function fetchfillMateriasAlumno() {
         break;
 
       case 404:
-        const error = (document.createElement("p").innerText =
-          "Esta carrera no tiene materias.");
+        let error = document.createElement("p");
+		error.innerText = "Esta carrera no tiene materias.";
+        error.classList.add("minecraft-p");
         document.getElementById("materias-wrapper").append(error);
 
         setTimeout(() => {
